95383feb808fe4d0489720c876e234b4e5285632
Alex Wong de Freitas <otaru1921@gmail.com>
2021-01-20 21:53:32 -0300

    Visitante vê a homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Get '/', to: 'home#index'
+  # Linha abaixo é igual
+  root 'home#index'
+  # Home Controller , Ação Index
 end
