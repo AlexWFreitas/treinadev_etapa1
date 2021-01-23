fa44c6b93fca4bff8ccf431ccabad71cd4ceb6b0
Alex Wong de Freitas <otaru1921@gmail.com>
2021-01-22 22:01:38 -0300

    Done: Cadastrar uma promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -12,7 +12,7 @@ class PromotionsController < ApplicationController
     end
 
     def create
-        @promotion = Promotion.new(name: "...", description: "...", code: "...",  discount_rate: "...", coupon_quantity: "...", expiration_date: "...")
+        @promotion = Promotion.new(promotion_params)
 
         if @promotion.save
             redirect_to @promotion
@@ -22,4 +22,9 @@ class PromotionsController < ApplicationController
         end
     end
 
+    private
+
+    def promotion_params
+        params.require(:promotion).permit(:name, :description, :code, :discount_rate, :coupon_quantity, :expiration_date)
+    end
 end
