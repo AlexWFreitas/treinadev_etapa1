3b9f07117b57b8e3bb2594d2baf8ef87e96e2783
Alex Wong de Freitas <otaru1921@gmail.com>
2021-01-27 20:10:59 -0300

    Add: Capybara test for Deletion

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
+  # As 2 linhas abaixo são iguais
   # Get '/', to: 'home#index'
-  # Linha abaixo é igual
   root 'home#index'
+  
   # Home Controller , Ação Index
+
   resources :promotions, only: [:index, :show, :create, :new, :edit, :update, :destroy]
 end
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- spec/features/6 - admin_deletes_a_promotion_spec.rb
+++ spec/features/6 - admin_deletes_a_promotion_spec.rb
@@ -18,3 +18,28 @@ describe Promotion do
     end
   end
 end
+
+feature 'Admin deletes a promotion' do
+  scenario 'delete an existing promotion' do
+    # Arrange
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+
+    Promotion.create!(name: 'Pascoa', description: 'Promoção de Pascoa',
+                      code: 'PASC10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '24/12/2033')    
+                      
+
+    # Act
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Natal'
+    click_on 'Deletar'
+
+    # Assert
+    expect(current_path).to eq promotions_path
+    expect(page).to_not have_content('Natal')
+    expect(page).to have_content('Pascoa')
+  end
+end
