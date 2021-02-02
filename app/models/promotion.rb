85ae45ecb96e85f07a51beb2e14dd0c09c4e3d53
Alex Wong de Freitas <otaru1921@gmail.com>
2021-02-01 22:10:24 -0300

    WIP: Gerador de cupons com problema?

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,4 +1,5 @@
 class Promotion < ApplicationRecord
+    has_many :coupons
     validates :name, :code, :discount_rate, :expiration_date, :coupon_quantity, presence: true
     validates :name, :code, uniqueness: true
     validates :discount_rate, :coupon_quantity,  numericality: true
