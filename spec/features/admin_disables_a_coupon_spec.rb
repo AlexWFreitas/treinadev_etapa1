93b344e119cbc57c8f49fca16fcf81d6919e33cf
Alex Wong de Freitas <otaru1921@gmail.com>
2021-02-05 20:33:39 -0300

    Updated disable coupon test to use selectors for div id

--- spec/features/admin_disables_a_coupon_spec.rb
+++ spec/features/admin_disables_a_coupon_spec.rb
@@ -12,7 +12,33 @@ feature 'Admin disables a coupon' do
         click_on I18n.t ('promotions.show.disable_coupon')
 
         expect(page).to have_content('ABC0001 (Inativo)')
+        expect(promotion.coupons.reload.active.size).to eq(0)
         coupon.reload.status
         expect(coupon.status).to eq('inactive')
+    end
+
+    scenario "disable button doesn't appear" do
+        promotion = Promotion.create!(  name: 'Natal', code: 'NATAL10', description: 'Feliz Natal', coupon_quantity: 1, 
+                            discount_rate: 20, expiration_date: '22/12/2033'    )
+        inactive_coupon = Coupon.create!(code: 'ABC0001', promotion: promotion, status: :inactive)
+
+        active_coupon = Coupon.create!(code: 'ABC0002', promotion: promotion, status: :active)
+
+        visit root_path
+        click_on I18n.t ('promotions.collection_name')
+        click_on promotion.name
+
+
+        expect(page).to have_content('ABC0001 (Inativo)')
+        expect(page).to have_content('ABC0002 (Ativo)')
+        within("div#coupon-#{active_coupon.id}") do
+            expect(page).not_to have_link(I18n.t '.promotions.show.enable_coupon')
+            expect(page).to have_link(I18n.t '.promotions.show.disable_coupon')
+        end
+
+        within("div#coupon-#{inactive_coupon.id}") do
+            expect(page).not_to have_link(I18n.t '.promotions.show.disable.coupon')
+            expect(page).to have_link(I18n.t '.promotions.show.enable_coupon')
+        end
     end
 end
