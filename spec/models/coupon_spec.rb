96ff457c61ca12ef2c509524b86dcf4be123d30b
Alex Wong de Freitas <otaru1921@gmail.com>
2021-02-04 23:44:21 -0300

    Minor edit because the extra code was unnecessary because the spec  is on models folder.

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Coupon, type: :model do
+describe Coupon do
     describe 'associations' do
         it { should belong_to(:promotion).class_name('Promotion') }
       end
